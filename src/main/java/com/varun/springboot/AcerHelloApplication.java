eb3c8ea07da5cfc18e663444523d27ba57c52be2
varun <varun9100@gmail.com>
2020-05-29 23:36:47 -0500

    First Commit. Dependency - web, controller - string

---
+++ src/main/java/com/varun/springboot/AcerHelloApplication.java
@@ -0,0 +1,13 @@
+package com.varun.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcerHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcerHelloApplication.class, args);
+	}
+
+}
